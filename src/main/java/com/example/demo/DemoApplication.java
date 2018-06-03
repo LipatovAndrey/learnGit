c75a99dcef9868ef527caa52bab770afdf01777f
lipa <lipa-andrej@yandex.ru>
2018-06-03 19:08:30 +0300

    init2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static java.lang.System.out;
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-
+        System.out.print("hello git");
     }
 }
